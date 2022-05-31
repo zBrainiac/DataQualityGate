787c1b2f403fc19c467b2f2964a34bad7a4a828a
brainiac <Schoof1+!?>
2022-05-31 19:03:25 +0200

    code refactoring add DF join

---
+++ src/main/scala/abc_SchemaValidator_join.scala
@@ -0,0 +1,90 @@
+import com.amazon.deequ.schema.{RowLevelSchema, RowLevelSchemaValidator}
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.rogach.scallop.{LazyMap, ScallopConf}
+
+object abc_SchemaValidator_join {
+
+  class Conf(args: Seq[String])
+    extends ScallopConf(args) {
+    val propsMap: LazyMap[String, String] = props[String]('P')
+    verify()
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val object_name = getClass.getName
+    val name = object_name.substring(0, object_name.length -1)
+    println("object_name: " + name)
+
+    val conf = new Conf(args)
+    val input_filename: String = conf.propsMap("input_filename")
+    val input_path: String = conf.propsMap("input_path")
+    val output_path: String = conf.propsMap("output_path")
+
+    println("input_filename is: " + input_filename)
+    println("input_path is: " + input_path)
+    println("output_path is: " + output_path)
+
+    val spark = SparkSession
+      .builder()
+      .appName(name)
+      .master("local")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    val dataset = spark.read
+      .option("header", value = true)
+      .option("delimiter", "\t")
+      .option("inferSchema", "true")
+      .csv(input_path + input_filename).toDF()
+
+    dataset.printSchema()
+
+    println("RAW dataset:")
+    dataset.show(10)
+
+    val lookup_star_rating = spark.read
+      .option("header", value = true)
+      .option("delimiter", ",")
+      .option("inferSchema", "true")
+      .csv(input_path + "lookup_star_rating,csv").toDF()
+
+    lookup_star_rating.printSchema()
+
+    println(s"\nlookup dataset:")
+    lookup_star_rating.show(10)
+
+
+    val enriched_dataset = dataset.join(lookup_star_rating,dataset("star_rating") === lookup_star_rating("star_rating")).select(dataset("*"),lookup_star_rating("description"))
+      .toDF
+
+    enriched_dataset.printSchema()
+    println(s"\nenriched dataset:")
+    enriched_dataset.show()
+
+    val schema = RowLevelSchema()
+      .withIntColumn("customer_id", isNullable = false)
+      .withStringColumn(name = "product_id", isNullable = false, matches = Some("^[A-Z0-9]+$"))
+      .withIntColumn("product_parent", isNullable = false)
+      .withStringColumn("vine", isNullable = false, matches = Option("N|Y"))
+      .withTimestampColumn("review_date", mask = "yyyy-MM-dd", isNullable = false)
+
+    val result = RowLevelSchemaValidator.validate(enriched_dataset, schema)
+
+    println(s"\nnumValidRows: " + result.numValidRows)
+    println(s"\nnumInvalidRows: " + result.numInvalidRows)
+
+    println(s"\nInvalid Rows:")
+    result.invalidRows.show(120, truncate = true)
+
+    result.validRows.write.mode(SaveMode.Overwrite)
+      .orc(output_path + name + "_valid_orc")
+
+    result.validRows.write.mode(SaveMode.Overwrite)
+      .csv(output_path + name +  "_valid_csv")
+
+    result.invalidRows.write.mode(SaveMode.Overwrite)
+      .csv(output_path + name + "_invalid")
+  }
+}
